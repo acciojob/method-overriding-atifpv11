8e2a69966933ef246ba8137cc62341f34ff621aa
atifpv11 <muhammedatif44@gmail.com>
2024-02-17 17:37:43 +0530

    added the files

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3: Create an object of class B and call method meth of class A with it
+        B objB = new B();
+        System.out.println(objB.meth()); // This will print the overridden method from class B
+        // Task 5: Call the overridden method from obj of class B
+        // Since the method is overridden in class B, the overridden version will be called
+    }
 }
